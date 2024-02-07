150e7fe0785ea2848f23e380be1338f250d251ac
spoofle <33384343+spoofle@users.noreply.github.com>
2024-02-07 10:06:56 -0500

    Set up login page, css style sheet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
+import LoginScreen from './LoginScreen';
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <p>
-          Hello.
-        </p>
+        <LoginScreen />
       </header>
     </div>
   );
